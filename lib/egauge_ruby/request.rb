8ec5b173462386dc9b1b9d2665d24d97df1b9000
Joshua Burke <burke.joshua.james@gmail.com>
2015-01-15 12:06:59 -0500

    request arguments and getting stored values

--- lib/egauge_ruby/request.rb
+++ lib/egauge_ruby/request.rb
@@ -7,21 +7,23 @@ require 'nokogiri'
 
 module EgaugeRuby
   class Register
-    attr_accessor :timestamp, :name, :type, :value
+    attr_accessor :timestamp, :name, :type, :value, :instantaneous
 
     def initialize(config = {})
       @timestamp      = config[:timestamp]
       @name           = config[:name]
       @type           = config[:type]
       @value          = config[:value]
+      @instantaneous  = config[:instantaneous]
     end
 
     def to_hash
       hash = {}
-      hash[:timestamp]  = timestamp
-      hash[:name]       = name
-      hash[:type]       = type
-      hash[:value]      = value
+      hash[:timestamp]        = timestamp
+      hash[:name]             = name
+      hash[:type]             = type
+      hash[:value]            = value
+      hash[:instantaneous]    = instantaneous
       hash
     end
   end
@@ -52,32 +54,67 @@ module EgaugeRuby
     #
     # want to get the measurements for the last 24 hours
     # want to calculate totals and maybe averages?
-    #
-    attr_accessor :url, :query_arguments, :registers,
-                  :document, :timestamp
 
-    def initialize(url, query_arguments=[])
-      @url, @query_arguments  = url, query_arguments
-      @document               = parse(get_current)
-      @timestamp              = Time.at(document.xpath('//ts').text.to_i)
-      @registers              = []
+    attr_accessor :base_url, :query_arguments, :full_url,
+                  :registers, :document, :timestamp
+
+    def initialize(base_url:, request_type: "current", query_arguments: [], config: {})
+      @base_url                     = base_url || config[:base_url]
+      @request_type                 = request_type || config[:request_type]
+      @query_arguments              = query_arguments || config[:query_arguments]
+      @full_url                     = join_url
+      @document                     = parse(get_current)
+      @timestamp                    = Time.at(document.xpath('//ts').text.to_i)
+      @registers                    = []
 
       set_registers
+    end
+
+    def join_query_args
+      accepted = %w{tot noteam v1 inst}
+      arguments = []
+      @query_arguments.each do |arg|
+        arguments.push(arg) if accepted.include?(arg)
+      end
+      "?" + arguments.join('&')
+    end
+
+    # Concatentate and Return:
+    # 1. base_url
+    # 2. query_type(current or hist)
+    # 3. string of query args
+    # Return String
+
+    def join_url
+      @base_url + join_query_args
     end
 
     # Get the current values for all registers defined
     #
     # 1. loops through the registers
-    # 2. converts the values to something sensible
+    # 2. converts the values to a Ruby Hash
     # 3. appends the each attriubute to a hash with name as key
     # 4. returns the hash
 
     def current
-      registers.map(&:to_hash)
+      results = {}
+      registers.each {|r| results[r.name] = r}
+      results
+    end
+
+    def get_stored
+      RestClient.get(full_url) do |response, request, result, &block|
+        case response.code
+        when 200
+          response
+        else
+          response.return!(request, result, &block)
+        end
+      end
     end
 
     def get_current
-      RestClient.get(url) do |response, request, result, &block|
+      RestClient.get(full_url) do |response, request, result, &block|
         case response.code
         when 200
           response
@@ -92,25 +129,29 @@ module EgaugeRuby
     end
 
     def set_registers
-      register_collection = document.xpath('//r')
-      register_collection.each { |reg| create_register_obj(reg)}
+      @registers = []
+      xml_register_collection = document.xpath('//r')
+      xml_register_collection.each do |xml_reg|
+        create_register_obj(xml_reg)
+      end
     end
 
-    def create_register_obj(reg)
-      new_reg = EgaugeRuby::Register.new(register_to_hash(reg))
-      add_register(new_reg)
+    def create_register_obj(xml_reg)
+      new_reg_obj = EgaugeRuby::Register.new(register_to_hash(xml_reg))
+      add_register(new_reg_obj)
     end
 
-    def add_register(reg)
-      registers.push(reg)
+    def add_register(reg_obj)
+      registers.push(reg_obj)
     end
 
     def register_to_hash(reg)
       hash = {}
-      hash[:timestamp]  = timestamp
-      hash[:name]       = reg.attributes["n"].value
-      hash[:type]       = reg.attributes["t"].value
-      hash[:value]      = reg.children.text.to_i
+      hash[:timestamp]      = timestamp
+      hash[:name]           = reg.attributes["n"].value
+      hash[:type]           = reg.attributes["t"].value
+      hash[:value]          = reg.xpath("v").text.to_i
+      hash[:instantaneous]  = reg.xpath("i").text.to_i
       hash
     end
   end
--- spec/egauge_ruby_spec.rb
+++ spec/egauge_ruby_spec.rb
@@ -4,11 +4,35 @@ module EgaugeRuby
   describe Request do
 
     let(:url) { "http://22north.egaug.es/cgi-bin/egauge" }
-    subject { EgaugeRuby::Request.new(url) }
+    let(:query_args) { ['v1', 'tot', 'inst'] }
+    subject { EgaugeRuby::Request.new(base_url: url, query_arguments: query_args) }
     let(:response) {subject.get_current }
 
     it "isn't nil when instantiated" do
       expect(subject.nil?).to eq(false)
+    end
+
+    describe "API arguments" do
+      it "can request current measurements or historical"
+
+      it "can specify the API version 1" do
+        data_element = subject.document.xpath('//data')
+
+        expect(data_element.class).to eq(Nokogiri::XML::NodeSet)
+        expect(data_element.children.length).to be >= 1
+      end
+
+      it "can specify whether to include totals" do
+        total_element = subject.document.xpath('//r[@rt="total"]').first
+
+        expect(total_element.attributes['rt'].value).to eq('total')
+      end
+
+      it "can request instantaneous values (power)" do
+        inst_element = subject.document.xpath('//r/i').first
+
+        expect(inst_element.name).to  eq('i')
+      end
     end
 
     describe '#get_current' do
@@ -22,7 +46,22 @@ module EgaugeRuby
       end
     end
 
-    let(:doc) {subject.current}
+    describe '#get_stored' do
+      let(:type) { "stored" }
+      subject { EgaugeRuby::Request.new(base_url: url, request_type: type) }
+
+      it "has a request URL containing 'egauge-show'" do
+        expect(subject.full_url).to include("egauge-show")
+      end
+
+      it "returns a 200 HTTP code" do
+        expect(subject.get_stored.code).to eq(200)
+      end
+
+      it "returns some XML" do
+        expect(subject.get_stored.body.length).to be > 20
+      end
+    end
 
     describe '#parse' do
       it '#parse turns XML into Ruby objects' do
@@ -32,6 +71,7 @@ module EgaugeRuby
 
     describe "#registers" do
       it "returns an array of the registers associated with the request"
+      it "has a unit attribute corresponding to the regiister type"
     end
 
     describe '#current' do
@@ -49,5 +89,13 @@ module EgaugeRuby
       it "clears the old registers"
       it "creates new registers for new response data"
     end
+  end
+
+  describe Register do
+    it "is instantiated to a non-nil value"
+    it "has a nil value for empty instance attributes"
+    it "has attributes equal to the xml values passed in"
+    it "has a timestamp equal to the register that created it"
+    it "has a unit attribute corresponding to the regiister type"
   end
 end
